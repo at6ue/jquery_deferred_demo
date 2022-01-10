4713f5c42509b9317d788a00685b2297d9dc8d10
atti <18576384+at6ue@users.noreply.github.com>
2022-01-10 22:02:13 +0900

    Refactor: Format

--- docs/app/core.js
+++ docs/app/core.js
@@ -1,24 +1,43 @@
 // @ts-check
 
-const code = document.getElementById('code');
-const log = document.getElementById('log');
-const version = document.getElementById('version');
-const sample = document.getElementById('sample');
+const code = document.getElementById("code");
+const log = document.getElementById("log");
+const version = document.getElementById("version");
+const sample = document.getElementById("sample");
 
 if (
   !(sample instanceof HTMLSelectElement) ||
   !(version instanceof HTMLSelectElement)
-) throw new TypeError();
+)
+  throw new TypeError();
 
 [...Array(7)].forEach((_, i) => {
-  const name = (i + 1).toString().padStart(2, '0');
+  const name = (i + 1).toString().padStart(2, "0");
   sample.add(new Option(name, name));
 });
 
 const jQueryResources = new Map([
-  ['1', { version: '1.12.4', integrity: 'sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=' }],
-  ['2', { version: '2.2.4', integrity: 'sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=' }],
-  ['3', { version: '3.6.0', integrity: 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' }]
+  [
+    "1",
+    {
+      version: "1.12.4",
+      integrity: "sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=",
+    },
+  ],
+  [
+    "2",
+    {
+      version: "2.2.4",
+      integrity: "sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=",
+    },
+  ],
+  [
+    "3",
+    {
+      version: "3.6.0",
+      integrity: "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=",
+    },
+  ],
 ]);
 
 jQueryResources.forEach((jq, majaor) => {
@@ -32,21 +51,21 @@ console.log = (incoming) => {
 };
 
 const reset = () => {
-  log.textContent = '';
-  code.textContent = '';
+  log.textContent = "";
+  code.textContent = "";
 };
 
-let currentJQueryMajorVersion = '';
+let currentJQueryMajorVersion = "";
 
 const loadJQuery = (ver) => {
   const jq = jQueryResources.get(ver);
-  if (typeof jq === 'undefined') throw new TypeError();
+  if (typeof jq === "undefined") throw new TypeError();
 
-  return fetch(
-    `https://code.jquery.com/jquery-${jq.version}.min.js`,
-    { integrity: jq.integrity })
-    .then(res => res.text())
-    .then(script => {
+  return fetch(`https://code.jquery.com/jquery-${jq.version}.min.js`, {
+    integrity: jq.integrity,
+  })
+    .then((res) => res.text())
+    .then((script) => {
       Function(script)();
       currentJQueryMajorVersion = ver;
     });
@@ -54,16 +73,17 @@ const loadJQuery = (ver) => {
 
 const start = () => {
   const ver = version.value;
-  const jQueryLoaded = currentJQueryMajorVersion === ver ?
-    Promise.resolve() : loadJQuery(ver);
+  const jQueryLoaded =
+    currentJQueryMajorVersion === ver ? Promise.resolve() : loadJQuery(ver);
 
   consoleLog(`== Sample ${sample.value} ==`);
   reset();
 
-  jQueryLoaded.then(() => fetch(`app/${sample.value}.js`))
-    .then(res => res.text())
-    .then(script => {
-      if (typeof $ === 'undefined') throw new ReferenceError();
+  jQueryLoaded
+    .then(() => fetch(`app/${sample.value}.js`))
+    .then((res) => res.text())
+    .then((script) => {
+      if (typeof $ === "undefined") throw new ReferenceError();
       code.textContent = script;
       Function(script)();
     });
